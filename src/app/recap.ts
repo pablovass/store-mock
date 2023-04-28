ed585f50d4b5ff52d64775cecc839d0e6a0c0f1d
pablovass <faktory.fm@live.com>
2023-04-28 19:23:46 -0300

    repaso de html y ts

--- src/app/recap.ts
+++ src/app/recap.ts
@@ -3,4 +3,20 @@ const sum(a:number,b:number)=>{
   return a+b;
 }
 
-sum();
+sum(1,3);
+
+//class Person{
+//  age:number;
+//  lastName:string;
+//  constructor(age:number,lastName:string){
+//    this.age=age;
+//    this.lastName=lastName;
+//
+//  }
+//}
+
+class Person {
+  constructor(private age:number,public lastName:string){}
+}
+const nico=new Person(15,"molina");
+//nico.age;
