e5d0f13647629e71cdd95feee62a6a1395428221
pablovass <faktory.fm@live.com>
2023-05-03 11:21:42 -0300

    arrancando desde cero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,82 +1,13 @@
-import { Component } from '@angular/core';
+/**
+Verifica versión de Node: node -v
 
-import { Product } from './product.model';
+Verifica versión de npm: npm -v
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  name = 'Nicolas';
-  age = 18;
-  img = 'https://www.w3schools.com/howto/img_avatar.png';
-  btnDisabled = true;
-  person = {
-    name: 'Nicolas',
-    age: 18,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
-  }
-  names: string[] = ['Nico', 'Juli', 'Santi'];
-  newName = '';
-  products: Product[] = [
-    {
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-      category: 'all',
-    },
-    {
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-    {
-      name: 'Casa para perro',
-      price: 34,
-      image: './assets/images/house.jpg'
-    },
-    {
-      name: 'Gafas',
-      price: 3434,
-      image: './assets/images/glasses.jpg'
-    }
-  ]
+Instala el CLI de Angular: npm -g @angular/cli
 
-  toggleButton() {
-    this.btnDisabled = !this.btnDisabled;
-  }
+Verifica tu instalación: ng version
 
-  increaseAge() {
-    this.person.age += 1;
-  }
+Crea tu primer proyecto: ng new my-project
 
-  onScroll(event: Event) {
-    const element = event.target as HTMLElement;
-    console.log(element.scrollTop);
-  }
-
-  changeName(event: Event) {
-    const element = event.target as HTMLInputElement;
-    this.person.name = element.value;
-  }
-
-  addName() {
-    this.names.push(this.newName);
-    this.newName = '';
-  }
-
-  deleteName(index: number) {
-    this.names.splice(index, 1);
-  }
-}
+Ejecuta el servidor de desarrollo: ng serve Dentro de la carpeta de tu proyecto.
+*/
