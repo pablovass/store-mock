401e1690314d0811278e2ee75d553268e9ea91e3
pablovass <faktory.fm@live.com>
2023-05-05 19:42:08 -0300

    guardo cambios pero hay que arreglar cosas

--- src/app/componets/img/img.component.ts
+++ src/app/componets/img/img.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Input } from '@angular/core';
 
+// los decoradores son todos los que tienen en su palabra @
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
-  styleUrls: ['./img.component.sass']
+  styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit{
+@Input() img:string='valor init';
+constructor(){}
+ngOnInit(): void {
 
+}
 }
