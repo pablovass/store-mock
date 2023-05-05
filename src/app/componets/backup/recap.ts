2a715aead19515c7db2d9666d633341ad6d4847f
pablovass <faktory.fm@live.com>
2023-05-05 16:23:38 -0300

    guardar backup

---
+++ src/app/componets/backup/recap.ts
@@ -0,0 +1,24 @@
+const username:string='pablobyte';
+const username1:string | number ='pablim';
+const username2 ='pablim';
+const sum = (a:number,b:number)=>{
+  return a+b;
+}
+
+sum(1,3);
+
+//class Person{
+//  age:number;
+//  lastName:string;
+//  constructor(age:number,lastName:string){
+//    this.age=age;
+//    this.lastName=lastName;
+//
+//  }
+//}
+
+class Person {
+  constructor(private age:number,public lastName:string){}
+}
+const nico=new Person(15,"molina");
+//nico.age;
