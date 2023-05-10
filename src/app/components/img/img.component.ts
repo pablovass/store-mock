586d9bf5697eddf441aa0fa2a48612065feec4c9
pablovass <faktory.fm@live.com>
2023-05-10 11:40:58 -0300

    ngDestroy and SetInput

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,52 +1,73 @@
-import { Component,OnInit,Input, Output,EventEmitter,OnChanges, SimpleChanges,AfterViewInit,OnDestroy} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
-// los decoradores son todos los que tienen en su palabra @
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
-  styleUrls: ['./img.component.scss']
+  styleUrls: ['./img.component.sass']
 })
-export class ImgComponent implements OnInit,OnChanges,AfterViewInit,OnDestroy{
-//@Input() img:string='valor init';
-@Input() img:string='';
-@Output()loaded=new EventEmitter<string>();
-imageDefault = './assets/images/default-img.jpg';
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-constructor(){
-    //before render
-  //NO async --once time
-  console.log('constructor','imgValue=>', this.img);
+  img: string = '';
 
-}
-ngOnChanges(){
-//ngOnChanges(changes: SimpleChanges): void {
-  //before render
-  //change inputs --times
-  // aca van los inputs y aca es donde se detectan los cambios
-  console.log('ngOnChanges','imgValue0>', this.img);
-}
-ngOnInit(): void {
-//no van los cambio de los input aca
-  //before render
-  // async - fetch -- once time
-  console.log('constructor','imgValue=>', this.img);
-}
-ngAfterViewInit(){
-  // after render
-  //handler children
-  console.log('AfterViewInit');
-}
-ngOnDestroy() {
-//delete
-}
-imgError() {
-  this.img = this.imageDefault;
-}
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    console.log('change just img  =>' ,this.img);
+    // code
+  }
+  @Input() alt: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = './assets/images/default-img.jpg';
+  counter = 0;
+  counterFn: number | undefined;
 
-imgLoaded() {
-  console.log('log hijo');
-  this.loaded.emit(this.img); //le esta mandando informacion al padre cuando es exitos el envio
-}
+  constructor() {
+    // before render
+    // NO async -- once time
+    // aca van los inputs y aca es donde se detectan los cambios
+    console.log('constructor', 'imgValue =>', this.img);
+  }
 
+  ngOnChanges(changes: SimpleChanges) {
+    // before - during render
+    // changes inputs -- multiples times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+    console.log('changes', changes);
+    // if (changes.) {
+    //   // code
+    // }
+  }
+
+  ngOnInit(): void {
+    //no van los cambio de los input aca
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+    this.counterFn = window.setInterval(() => {
+      this.counter += 1;
+      console.log('run counter');
+    }, 1000);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // handler children -- once time
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    // delete -- once time
+    console.log('ngOnDestroy');
+    window.clearInterval(this.counterFn);
+  }
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log('log hijo');
+    this.loaded.emit(this.img);  //le esta mandando informacion al padre cuando es exitos el envio
+  }
 
 }
