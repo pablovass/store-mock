8b0e6d866269cf5c5116dc3c1c619d125bf295c9
pablovass <faktory.fm@live.com>
2023-05-16 18:08:33 -0300

    refactor in product

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,25 +1,24 @@
-import { Component,OnInit,Input } from '@angular/core';
-//import {Product}from 'src/app/models/product.model';
-import {Product}from '../../models/product.model';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Product } from '../../models/product.model';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent  implements OnInit{
+export class ProductComponent implements OnInit {
 
-  @Input() product: Product={
-  id:'',
-  price: 0,
-  image:'',
-  name:''
-};
+  @Input() product: Product = {
+    id: '',
+    price: 0,
+    image: '',
+    name: ''
+  };
 
-constructor(){
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
-ngOnInit(): void {
-
-}
-}
-
