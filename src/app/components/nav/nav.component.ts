3fe3ca5b787b64bf9d1adf32cd746580eebf79ee
pablovass <faktory.fm@live.com>
2023-05-17 11:28:18 -0300

    nav working

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  activeMenu = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
 
 }
